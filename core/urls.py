511332e904c9e257325f1d094bd827972d76709c
Temur <temurbekqurbonov01tuit@gmail.com>
2025-08-27 23:13:36 +0500

    update

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -44,6 +44,12 @@ urlpatterns = [
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # JSON schema
+    re_path(r'^swagger\.json$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+
+    # YAML schema
+    re_path(r'^swagger\.yaml$', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
 
 ]
 
