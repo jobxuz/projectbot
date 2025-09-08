52d62ad89375d65144228f4e11d55d44056f759e
Baxtiyor Ergashev <b.ergashev@uic.group>
2025-09-08 09:13:37 +0500

    update

--- apps/application/migrations/0031_add_slider_order_field.py
+++ apps/application/migrations/0031_add_slider_order_field.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("application", "0029_add_slider_order_and_package_description"),
+        ("application", "0030_merge_20250908_0401"),
     ]
 
     operations = [
